5aa1e1891f2dcd4ce3282502532c8f6dab7a21b9
Felipe Cancino <jfelipecr94@gmail.com>
2024-05-03 21:07:47 -0400

    add icons classes

--- src/components/PrincipalType/PrincipalType.js
+++ src/components/PrincipalType/PrincipalType.js
@@ -3,7 +3,26 @@ import '../Sprites/Sprites.css';
 const PrincipalType = () => {
   return (
     <div className="principal_type">
-      <div className="pokemon-table type--agua"></div>
+      <div className="pokemon-table type--bicho"></div>
+      {/* <div className="pokemon-table type--siniestro"></div>
+      <div className="pokemon-table type--dragon"></div>
+      <div className="pokemon-table type--electrico"></div>
+      <div className="pokemon-table type--hada"></div>
+      <div className="pokemon-table type--lucha"></div>
+
+      <div className="pokemon-table type--fuego"></div>
+      <div className="pokemon-table type--volador"></div>
+      <div className="pokemon-table type--fantasma"></div>
+      <div className="pokemon-table type--planta"></div>
+      <div className="pokemon-table type--tierra"></div>
+      <div className="pokemon-table type--hielo"></div>
+
+      <div className="pokemon-table type--normal"></div>
+      <div className="pokemon-table type--veneno"></div>
+      <div className="pokemon-table type--psiquico"></div>
+      <div className="pokemon-table type--roca"></div>
+      <div className="pokemon-table type--acero"></div>
+      <div className="pokemon-table type--agua"></div> */}
     </div>
   );
 };
