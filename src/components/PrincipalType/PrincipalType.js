c7c57ae505909ff65bfe0557044b63fc5b5b104e
lauralop11 <lauralop_11@hotmail.com>
2024-05-04 20:17:56 -0400

    estilos tipos pokemon

--- src/components/PrincipalType/PrincipalType.js
+++ src/components/PrincipalType/PrincipalType.js
@@ -1,30 +1,36 @@
 import '../Sprites/Sprites.css';
+import './PrincipalType.css';
 
 const PrincipalType = () => {
   return (
     <div className="principal_type">
       <div className="pokemon-table type--bicho"></div>
-      {/* <div className="pokemon-table type--siniestro"></div>
-      <div className="pokemon-table type--dragon"></div>
-      <div className="pokemon-table type--electrico"></div>
-      <div className="pokemon-table type--hada"></div>
-      <div className="pokemon-table type--lucha"></div>
+      <div className="pokemon-datos">
+          <p className='title-eficaz'>Eficaz</p>
+          <span className='pokemon-eficaz'> 
+            <div className="pokemon-datos-img type--bicho"></div>
+          </span>
+          <p className='title-debil'>Debil</p>
+          <span className='pokemon-debil'></span>
+          <p className='title-vulnerable'>Eficaz</p>
+          <span className='pokemon-vulnerable'></span>
+          <p className='title-inmune'>Debil</p>
+          <span className='pokemon-inmune'></span>
+      
+      {/*   
+        <div>
+          <p className='pokemon-title-type'>Eficaz</p>
+          <span className='pokemon-vulnerable'></span>
+        </div>
+        <div>
+          <p className='pokemon-title-type'>Eficaz</p>
+          <span className='pokemon-inmune'></span>
+        </div> */}
 
-      <div className="pokemon-table type--fuego"></div>
-      <div className="pokemon-table type--volador"></div>
-      <div className="pokemon-table type--fantasma"></div>
-      <div className="pokemon-table type--planta"></div>
-      <div className="pokemon-table type--tierra"></div>
-      <div className="pokemon-table type--hielo"></div>
-
-      <div className="pokemon-table type--normal"></div>
-      <div className="pokemon-table type--veneno"></div>
-      <div className="pokemon-table type--psiquico"></div>
-      <div className="pokemon-table type--roca"></div>
-      <div className="pokemon-table type--acero"></div>
-      <div className="pokemon-table type--agua"></div> */}
+      </div>
     </div>
   );
 };
+
 
 export { PrincipalType };
